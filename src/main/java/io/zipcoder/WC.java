565fb746ec81a190dd9bf4214643066145944ff3
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-16 10:36:43 -0400

    word counting is good, working on displaying by count

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,12 +2,14 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class WC {
 
     private Iterator<String> si;
+    private LinkedHashMap<String, Integer> linkedHashMap;
+    private final String DELIMITER = "[^a-zA-Z0-9]+";
 
     public WC(String fileName) {
         try {
@@ -16,16 +18,42 @@ public class WC {
             System.out.println(fileName + " Does Not Exist");
             System.exit(-1);
         }
+        linkedHashMap = new LinkedHashMap<String, Integer>();
     }
 
     public WC(Iterator<String> si) {
         this.si = si;
+        linkedHashMap = new LinkedHashMap<String, Integer>();
+    }
+
+    public LinkedHashMap<String, Integer> getLinkedHashMap() {
+        return linkedHashMap;
+    }
+
+    public void addWordsToLinkedHashMap() {
+//        System.out.println("test");
+
+        while(si.hasNext()) {
+            String[] words = si.next().split(DELIMITER);
+            for (String word : words) {
+                if (linkedHashMap.get(word) == null) {
+                    linkedHashMap.put(word, 1);
+                } else {
+                    linkedHashMap.put(word, linkedHashMap.get(word) + 1);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
 
         String inputExpression = "first second third fourth";
-
+        WC wc = new WC(WC.class.getResource("/aeneid.txt").getFile());
+        wc.addWordsToLinkedHashMap();
+        wc.getLinkedHashMap();
+        for (Map.Entry entry : wc.getLinkedHashMap().entrySet()) {
+            System.out.println(entry.toString());
+        }
 
     }
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -1,11 +1,36 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class WCTest {
+
+    private WC wc;
+
+    @Before
+    public void setupConstructorIteratorTest() {
+        // Given
+        String expectedFirstWord = "first";
+        String wordsAsString = "first second third fourth";
+        String[] wordsArray = wordsAsString.split("[a-zA-Z0-9]+");
+        List<String> wordsArrayList = new ArrayList<String>(Arrays.asList(wordsArray));
+        Iterator<String> wordsArrayListIterator = wordsArrayList.iterator();
+        wc = new WC(wordsArrayListIterator);
+        // When
+        String actualFirstWord = "hold";
+        // Then
+    }
+
+    @Test
+    public void createWordsArrayTest() {
+
+    }
+
 
 }
